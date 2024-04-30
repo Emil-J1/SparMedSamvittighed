fdce09879ab6484b93693a0467d250fd45f5b421
Mathias Ritter <huskmigffs@gmail.com>
2024-04-30 13:08:54 +0200

    Lets goooo

---
+++ spar-med-samvittighed/src/app/products/page.tsx
@@ -0,0 +1,18 @@
+export default function Products()
+{
+    return( <main className="flex min-h-screen flex-col items-center justify-between p-24"> <div>
+        <h1>Products</h1>
+        <ul>
+            <li>
+                Product
+            </li>
+            <li>
+                Product
+            </li>
+            <li>
+                Product
+            </li>
+        </ul>
+        </div> </main>);
+}
+
