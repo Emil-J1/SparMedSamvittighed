cf5c2524eb0c735939d62667ce1f1b00d7dd074b
dishaan2000 <93879978+dishaan2000@users.noreply.github.com>
2024-04-30 13:56:53 +0200

    "Update Login page with improved styling and layout"
    This commit updates the Login page with the following changes:
    
    * Adds a gray background color to the main container.
    * Wraps the form elements in a separate white

--- spar-med-samvittighed/src/app/Login/page.tsx
+++ spar-med-samvittighed/src/app/Login/page.tsx
@@ -1,13 +1,24 @@
 export default function Login() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-100">
+      <div className="max-w-md w-full bg-white rounded shadow-md p-6">
+        <h1 className="text-3xl font-bold mb-4 text-gray-700 text-center">Login</h1>
         <div>
-          <h1>Login</h1>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button className="btn mt-8">Log in</button>
+          <input
+            type="text"
+            placeholder="Username"
+            className="w-full p-2 pl-10 text-sm text-gray-700"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full p-2 pl-10 text-sm text-gray-700 mt-4"
+          />
         </div>
-      </main>
-    );
-  }
-  
+        <button className="btn mt-8 w-full bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+          Log in
+        </button>
+      </div>
+    </main>
+  );
+}
