85e009296b8b6306001e67fcb9fa74c314585e76
dishaan2000 <93879978+dishaan2000@users.noreply.github.com>
2024-05-07 10:11:08 +0000

    clean up

--- spar-med-samvittighed/models/address.ts
+++ spar-med-samvittighed/models/address.ts
@@ -1,6 +1,15 @@
-import mongoose from "mongoose";
+import { Schema, model } from 'mongoose';
 
-const addressSchema = new mongoose.Schema({
+interface Store {
+  street_address: String;
+  state: String;
+  postal_code: String;
+  country: String;
+  location: String;
+
+}  
+
+const addressSchema = new Schema<address>({
   street_address: String, // Street address
   city: String, // City name
   state: String, // State or province abbreviation (optional)
@@ -9,7 +18,7 @@ const addressSchema = new mongoose.Schema({
     validate: {
       validator: function(value: string): boolean {
         // Replace with your specific postal code format regex (if needed)
-        return /^\d{5}$/.test(value); // Example for 5-digit US postal codes
+        return /^\d{4}$/.test(value); // Example for 4-digit DKK postal codes
       },
       message: 'Invalid postal code format'
     }
@@ -21,4 +30,5 @@ const addressSchema = new mongoose.Schema({
   }
 });
 
-export default mongoose.model('Address', addressSchema); // Export the model
+
+export const Address = model<Address>('Address', addressSchema);
