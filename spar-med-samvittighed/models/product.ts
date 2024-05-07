2921e1d84af20b0ee2a9cbadbcfc4ccb589abc20
Sebastian Christensen <sebc8@hotmail.com>
2024-05-07 13:25:24 +0200

    validation

--- spar-med-samvittighed/models/product.ts
+++ spar-med-samvittighed/models/product.ts
@@ -8,23 +8,46 @@ interface Product {
   image: string;
 }
 
+const IMAGE_URL_PREFIX = "https://digitalassets.sallinggroup.com";
+
 const ProductSchema = new Schema<Product>({
   ean: {
     type: Number,
     unique: true,
     required: true,
+    match: [
+      /^\d{13}$/,
+      "EAN must be exactly 13 digits long and consist only of numbers.",
+    ],
   },
   category: {
     type: String,
     required: true,
+    match: [
+      /^[a-zA-Z0-9\s]+$/,
+      "Category must contain only alphanumeric characters and spaces.",
+    ],
   },
+
   description: {
     type: String,
     required: true,
+    match: [
+      /^[a-zA-Z0-9\s]+$/,
+      "Description must contain only alphanumeric characters and spaces.",
+    ],
   },
   image: {
     type: String,
-    required: true,
+    required: false,
+    match: [/^https?:\/\/.+$/, "Category must be a valid URL."],
+    validate: {
+      validator: function (v: string) {
+        // Validate that the image URL starts with the specified prefix
+        return v.startsWith(IMAGE_URL_PREFIX);
+      },
+      message: "error",
+    },
   },
 });
 
