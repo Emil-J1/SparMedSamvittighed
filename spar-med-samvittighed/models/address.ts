bcce3dbb612d2d9e32acfd056b2db245e60e69a7
Emil-J1 <10407718@ucn.dk>
2024-05-08 13:44:30 +0200

    Added-regex-to-address-model-finished-v2

--- spar-med-samvittighed/models/address.ts
+++ spar-med-samvittighed/models/address.ts
@@ -4,33 +4,43 @@ interface Address {
   street_address: string;
   city: string;
   state: string;
-  postal_code: string;
+  postal_code: number;
   country: string;
-  location?: { // Optional location details (consider using GeoJSON for geospatial data)
-    type: { type: string; default: "Point" };
-    coordinates: number[];
-  };
-}  
+  coordinates: number;
+};
 
 const AddressSchema = new Schema<Address>({
-  street_address: String, // Street address
-  city: String, // City name
-  state: String, // State or province abbreviation (optional)
-  postal_code: {
+  street_address: {
     type: String,
-    validate: {
-      validator: function(value: string): boolean {
-        // Replace with your specific postal code format regex (if needed)
-        return /^\d{4}$/.test(value); // Example for 4-digit DKK postal codes
-      },
-      message: 'Invalid postal code format'
-    }
+    match: [/^[a-øA-Ø0-9\s]+$/, "Street address invalid, only charactors and numbers allowed."], // Street address (only characters and numbers)
+    required: [true, "Street address is required."],
+  
+  }, // Street address
+  city:{
+    type: String,
+    match: [/^[a-øA-Ø]+$/, "City invalid, only charactors allowed."], // City name (only characters)
+    required: [true, "City is required."],
+  }, // City name
+  state: {
+    type: String,
+    match: [/^[a-øA-Ø]+$/, "State invalid, only charactors allowed."], // State name (only characters)
+    required: [false, "State is optional."],
   },
-  country: String, // Country name
-  location: { // Optional location details (consider using GeoJSON for geospatial data)
-    type: { type: String, default: "Point" },
-    coordinates: [Number]
-  }
+  postal_code: {
+    type: Number,
+    match: [/^\d{4}$/, "Postal-code invalidt, only 4 digits is allowed."], // Postal code (4 digits)
+    required: [true, "Postal code is required."],
+  },
+  country: {
+    type: String,
+    match: [/^[a-øA-Ø]+$/, "Country invalid, only charactors allowed."], // Country name (only characters)
+    required: [true, "Country is required."],
+  },
+  coordinates: {
+    type: Number,
+    match: [/^[-+]?\d{1,3}(?:\.\d+)?,\s*[-+]?\d{1,3}(?:\.\d+)?$/, "Coordinates invalid."], // Coordinates (6 digits)
+    required: [true, "Coordinates is required."],
+  },
 });
 
 
