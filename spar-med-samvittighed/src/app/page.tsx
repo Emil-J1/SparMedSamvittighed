ce4ccd89c1284162970be5fdea99f057b4853437
Emil-J1 <10407718@ucn.dk>
2024-05-15 11:32:05 +0200

    Added-pages-folder-AND-created-api-folder-with-example-api-call-inside

--- spar-med-samvittighed/src/app/page.tsx
+++ spar-med-samvittighed/src/app/page.tsx
@@ -1,11 +1,7 @@
-import { useClient } from '../../.next/';
-import YourComponent from "./comps/Api";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      <YourComponent />
+      <h1>Hej</h1>
     </main>
   );
 }
