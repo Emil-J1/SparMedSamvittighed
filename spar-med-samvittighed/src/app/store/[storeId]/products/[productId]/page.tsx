b9e576ad28b6b82cd33d5125b5cee07bdcc73f65
Emil-J1 <10407718@ucn.dk>
2024-05-21 12:34:28 +0200

    Hotfix: changed-components-folder-name AND changed-components-file-name

--- spar-med-samvittighed/src/app/store/[storeId]/products/[productId]/page.tsx
+++ spar-med-samvittighed/src/app/store/[storeId]/products/[productId]/page.tsx
@@ -1,4 +1,4 @@
-import ProductPage from "@/comps/Product";
+import Product from "@/components/Product";
 
 interface ProductProp {
   params: {
@@ -11,7 +11,7 @@ export default function ProductDetails({ params }: ProductProp) {
   return (
     <section className="flex min-h-screen flex-col items-center justify-between p-24 border rounded-lg">
       <h1>Info about the product {params.productId}</h1>
-      <ProductPage storeId={params.storeId} productId={params.productId} />
+      <Product storeId={params.storeId} productId={params.productId} />
     </section>
   );
 }
