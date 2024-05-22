8676dd3d9f0ae7dd910a0897d98e30398757f3d4
Emil-J1 <10407718@ucn.dk>
2024-05-22 14:36:59 +0200

    Feat: final-adjustments-on-search-component

--- spar-med-samvittighed/src/app/store/page.tsx
+++ spar-med-samvittighed/src/app/store/page.tsx
@@ -1,10 +1,20 @@
+"use client"
+import React, { useState } from "react";
+import Searchbar from "@/components/Searchbar";
 import StoreList from "@/components/StoreList";
 
 export default function StoreListPage() {
+  const [zipCode, setZipCode] = useState<string | null>(null);
+
+  const handleSearch = (zipCode: string) => {
+    setZipCode(zipCode);
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="flex justify-center text-3xl pb-12">Butikker</h1>
-      <StoreList />
+      <Searchbar onSearch={handleSearch} />
+      {zipCode ? <StoreList zipCode={zipCode} /> : <StoreList zipCode='9000' />}
     </div>
   );
 }
