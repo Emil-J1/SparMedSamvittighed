3d8911935e567a45ffcaa3759e6457c1547d6fd4
Emil-J1 <10407718@ucn.dk>
2024-05-23 12:54:41 +0200

    Hotfix: Styled login page

--- spar-med-samvittighed/src/app/auth/login/page.tsx
+++ spar-med-samvittighed/src/app/auth/login/page.tsx
@@ -7,15 +7,15 @@ export default function Login() {
   const [error, formAction] = useFormState(loginAction, undefined);
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form action={formAction}>
-        <input type="email" name="email" />
-        <input type="password" name="password" />
-        <button type="submit">Login</button>
+    <section className="flex flex-col justify-center items-center py-20">
+      <h1 className="text-2xl font-bold mb-4">Login</h1>
+      <form action={formAction} className="flex flex-col space-y-4 w-1/3">
+        <input type="email" placeholder="Email" name="email" className="border border-gray-300 p-2 rounded" />
+        <input type="password" placeholder="Password" name="password" className="border border-gray-300 p-2 rounded" />
+        <button type="submit" className="bg-blue-500 hover:bg-blue-400 text-white py-2 px-4 rounded w-1/2 self-center">Login</button>
       </form>
 
-      {error && <p>{error}</p>}
-    </div>
+      {error && <p className="text-red-500">{error}</p>}
+    </section>
   );
 }
