cd0a706ec71a93a95d9e55b7a5cb26bbca49a0ba
Emil-J1 <10407718@ucn.dk>
2024-05-23 09:30:55 +0200

    Hotfix: Updated searchbar submit button width

--- spar-med-samvittighed/src/components/Searchbar.tsx
+++ spar-med-samvittighed/src/components/Searchbar.tsx
@@ -18,23 +18,43 @@ const Searchbar: React.FC<SearchbarProps> = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex justify-center items-center mb-10 h-12 w-full">
+    <form
+      onSubmit={handleSubmit}
+      className="flex justify-center items-center mb-10 h-12 w-full"
+    >
       <input
         type="text"
-        placeholder="Søg efter produkter"
+        placeholder="Postnummer"
         value={inputValue}
         onChange={handleInputChange}
+        onKeyDown={(event) => {
+          const pattern = /[0-9]/;
+          if (
+            !pattern.test(event.key) &&
+            event.key !== "Backspace" &&
+            event.key !== "Delete"
+          ) {
+            event.preventDefault();
+          }
+        }}
+        onInput={(event) => {
+          if ((event.target as HTMLInputElement).value.length > 4) {
+            (event.target as HTMLInputElement).value = (
+              event.target as HTMLInputElement
+            ).value.slice(0, 4);
+          }
+        }}
         className="border border-gray-400 p-2 rounded-lg text-black h-full w-full"
       />
-      <button type="submit" className="bg-green-600 hover:bg-green-500 text-black p-2 rounded-lg ml-2 h-full">
-        <div className="flex flex-row items-center h-full">
-          Søg
-          <img
-            className="h-full"
-            src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"
-            alt="Søge-ikon"
-          />
-        </div>
+      <button
+        type="submit"
+        className="bg-green-600 hover:bg-green-500 text-black p-2 rounded-lg ml-2 h-full flex flex-row items-center w-14"
+      >
+        <img
+          className="h-full"
+          src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"
+          alt="Søge-ikon"
+        />
       </button>
     </form>
   );
