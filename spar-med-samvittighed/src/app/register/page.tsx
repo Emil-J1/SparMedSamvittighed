1e496c8727028e8792925a39eacb53c373bdc8a3
Emil-J1 <10407718@ucn.dk>
2024-05-27 10:30:03 +0200

    Styling: Styling the register page

--- spar-med-samvittighed/src/app/register/page.tsx
+++ spar-med-samvittighed/src/app/register/page.tsx
@@ -1,10 +1,14 @@
-"use client"
-import { useState } from 'react';
-
+"use client";
+import { useState } from "react";
 
 export default function Register() {
-  const [formData, setFormData] = useState({ email: '', username: '', password: '', zipCode: "",  });
-  const [message, setMessage] = useState('');
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    zipCode: "",
+  });
+  const [message, setMessage] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -14,10 +18,10 @@ export default function Register() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/register', {
-        method: 'POST',
+      const response = await fetch("/api/register", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -26,17 +30,17 @@ export default function Register() {
       if (response.ok) {
         setMessage(data.message);
       } else {
-        setMessage(data.message || 'Registration failed');
+        setMessage(data.message || "Registration failed");
       }
     } catch (error) {
-      setMessage('An unexpected error occurred');
+      setMessage("An unexpected error occurred");
     }
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-100">
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
+    <section className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-black">
+      <h1 className="text-4xl font-bold mb-8">Register</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col space-y-4 w-1/5 ">
         <input
           type="email"
           name="email"
@@ -44,6 +48,7 @@ export default function Register() {
           value={formData.email}
           onChange={handleChange}
           required
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
         />
         <input
           type="text"
@@ -52,6 +57,7 @@ export default function Register() {
           value={formData.username}
           onChange={handleChange}
           required
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
         />
         <input
           type="password"
@@ -60,20 +66,42 @@ export default function Register() {
           value={formData.password}
           onChange={handleChange}
           required
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
         />
         <input
-        type="text"
-        name="zipCode"
-        placeholder="Zip Code"
-        value={formData.zipCode}
-        onChange={handleChange}
-        required
+          type="text"
+          name="zipCode"
+          placeholder="Zip Code"
+          value={formData.zipCode}
+          onChange={handleChange}
+          onKeyDown={(event) => {
+            const pattern = /[0-9]/;
+            if (
+              !pattern.test(event.key) &&
+              event.key !== "Backspace" &&
+              event.key !== "Delete"
+            ) {
+              event.preventDefault();
+            }
+          }}
+          onInput={(event) => {
+            if ((event.target as HTMLInputElement).value.length > 4) {
+              (event.target as HTMLInputElement).value = (
+                event.target as HTMLInputElement
+              ).value.slice(0, 4);
+            }
+          }}
+          required
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
         />
-
-
-        <button type="submit">Register</button>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Register
+        </button>
       </form>
-      {message && <p>{message}</p>}
-    </div>
+      {message && <p className="mt-4 text-red-500">{message}</p>}
+    </section>
   );
 }
