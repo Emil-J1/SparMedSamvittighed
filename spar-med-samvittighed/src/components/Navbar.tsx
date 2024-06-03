ca1e521f58262e6b8182a47de769b63ffd9e0c5b
Mathias <huskmigffs@gmail.com>
2024-06-03 10:14:54 +0200

    logout implemented

--- spar-med-samvittighed/src/components/Navbar.tsx
+++ spar-med-samvittighed/src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import logoutAction from "../serveractions/auth/logoutaction";
 
 export default function Navbar() {
+
+
   return (
     <nav className="w-screen flex justify-between text-gray-800 px-10">
       <h1 className="font-bold">Spar med Samvittighed</h1>
       <Link href="/stores">Søg</Link>
+      <form action={logoutAction}>
+      <button type="submit">Logout</button>
+    </form>
     </nav>
   );
 }
